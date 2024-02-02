41e17907e5dbd435f3f18400cc17a2ee86c33d56
arberejupi <ae47279@ubt-uni.net>
2024-02-02 08:31:25 +0100

    Second Commit

---
+++ StarlabsCRUD/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarlabsCRUD.Data;
+using StarlabsCRUD.Models;
+namespace StarlabsCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PersonController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
+        {
+            return await _context.Person.ToListAsync();
+        }
+
+    }
+}
---
+++ StarlabsCRUD/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarlabsCRUD.Models;
+namespace StarlabsCRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
